5c8ee7244baede9f3468985b6c3544a5a90f6710
Hema <Hema@DESKTOP-ELR3GNH>
2022-02-06 20:02:16 +0530

    Initial Commit- upload Assignment1

--- Test/src/pattern/Pattern_1.java
+++ Test/src/pattern/Pattern_1.java
@@ -1,20 +1,21 @@
-package pattern_task;
+package pattern;
 
 import java.util.Scanner;
 
-public class Pattern_2 {
+public class Pattern_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Number : ");
+        System.out.print("Enter the Number: ");
         int num = sc.nextInt();
 
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= num; j++) {
-                System.out.print(i);
+                System.out.print("*");
             }
             System.out.println();
         }
 
         sc.close();
     }
+
 }
