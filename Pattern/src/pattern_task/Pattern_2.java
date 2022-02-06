292b674fe676d5eefa2fd5c8fb04ee3095ed8f78
Hema <Hema@DESKTOP-ELR3GNH>
2022-02-06 19:43:48 +0530

    Initial Commit - upload Assignment1

--- Pattern/src/pattern_task/Pattern_2.java
+++ Pattern/src/pattern_task/Pattern_2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Pattern_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Number: ");
+        System.out.print("Enter the Number : ");
         int num = sc.nextInt();
 
         for (int i = 1; i <= num; i++) {
--- Pattern/src/pattern_task/Pattern_3.java
+++ Pattern/src/pattern_task/Pattern_3.java
@@ -1,10 +1,21 @@
 package pattern_task;
 
+import java.util.Scanner;
+
 public class Pattern_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = sc.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-	}
-
+        sc.close();
+    }
 }
+
